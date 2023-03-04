80ff383486ce769f74ef9ccee33cdf6cc66f29ab
Shashank Siripragada <siripragada.s@northeastern.edu>
2023-03-04 00:32:24 -0500

    Bigram model

--- BLM.py
+++ BLM.py
@@ -9,19 +9,22 @@ class BLModel(nn.Module):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_len, vocab_len)
 
-    def forward(self, x, y):
+    def forward(self, x, y=None):
         logits = self.token_embedding_table(x)
-        B, T, C = logits.shape
-        logits = logits.view(B*T, C)
-        y = y.view(B*T)
-        loss = F.cross_entropy(logits, y)
+        if y is None:
+            loss = None
+        else:
+            b, t, c = logits.shape
+            logits = logits.view(b*t, c)
+            y = y.view(b*t)
+            loss = F.cross_entropy(logits, y)
         return logits, loss
 
     def generate(self, idx, max_tokens):
         for _ in range(max_tokens):
             logits, loss = self(idx)
-            probs = F.softmax(logits, dim=1)
-            
-
-
-
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
