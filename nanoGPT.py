eec30bde6276e098206c1e88166af36f94b64986
Shashank Siripragada <siripragada.s@northeastern.edu>
2023-03-04 03:53:11 -0500

    Update nanoGPT.py

--- nanoGPT.py
+++ nanoGPT.py
@@ -1,36 +1,5 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-# hyperparams
-batch_size = 32
-block_size = 8
-max_iters = 3000
-eval_interval = 300
-learning_rate = 1e-2
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-eval_iters = 200
-n_embed = 32
-# -----------------
-
-torch.manual_seed(1337)
-
-with open('input.txt', 'r', encoding='utf-8') as fp:
-    text = fp.read()
-
-chars = sorted(list(set(text)))
-vocab_len = len(chars)
-stoi = {ch: i for i, ch in enumerate(chars)}
-itos = {i: ch for i, ch in enumerate(chars)}
-encode = lambda s: [stoi[c] for c in s]
-decode = lambda l: "".join([itos[i] for i in l])
-
-encoded_text = encode(text)
-data = torch.tensor(encoded_text, dtype=torch.long)
-n = int(0.9*len(data))
-train = data[:n]
-val = data[n:]
+from BLM import BLModel
 
 
 def get_batch(split):
@@ -41,7 +10,90 @@ def get_batch(split):
     x, y = x.to(device), y.to(device)
     return x, y
 
+
 @torch.no_grad()
-def estimate_loss():
+def estimate_loss(model):
     out = {}
-    
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            _x, _y = get_batch(split)
+            _logits, _loss = model(_x, _y)
+            losses[k] = _loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+def train_model():
+    optimizer = torch.optim.AdamW(m.parameters(), lr=learning_rate)
+    for i in range(max_iters):
+        if i % eval_interval == 0:
+            _out = estimate_loss(m)
+            print(f"step {i}: train loss {_out['train']:.4f} val loss {_out['val']:.4f}")
+        optimizer.zero_grad()
+        x, y = get_batch('train')
+        logits, loss = m(x, y)
+        loss.backward()
+        optimizer.step()
+
+
+def generate():
+    idx = torch.zeros((1, 1), dtype=torch.long, device=device)
+    gens = decode(m.generate(idx, max_tokens=300)[0].tolist())
+    return gens
+
+
+def encode(string):
+    encoded = [stoi[char] for char in string]
+    return encoded
+
+
+def decode(indices):
+    return "".join([itos[i] for i in indices])
+
+
+if __name__ == '__main__':
+
+    # hyperparams
+    batch_size = 32
+    block_size = 8
+    max_iters = 3000
+    eval_interval = 300
+    learning_rate = 1e-2
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    eval_iters = 200
+    n_embed = 32
+    # -----------------
+
+    torch.manual_seed(1337)
+
+    with open('input.txt', 'r', encoding='utf-8') as fp:
+        text = fp.read()
+
+    chars = sorted(list(set(text)))
+    vocab_len = len(chars)
+    stoi = {ch: i for i, ch in enumerate(chars)}
+    itos = {i: ch for i, ch in enumerate(chars)}
+
+    encoded_text = encode(text)
+    data = torch.tensor(encoded_text, dtype=torch.long)
+    n = int(0.9 * len(data))
+    train = data[:n]
+    val = data[n:]
+
+    m = BLModel(vocab_len)
+    m = m.to(device)
+
+    train_model()
+    output = generate()
+    print(output)
+
+
+
+
+
+
+
+
